d1ba3870b25a92c539e713bf40f46224f718c0bc
hrosenskjold <47818937+hrosenskjold@users.noreply.github.com>
2024-07-10 10:55:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
 import json
 import requests
+import zipfile
+import shapefile  # pyshp library
+from shapely.geometry import shape, mapping
 from flask import Flask, request, jsonify, render_template
 from werkzeug.utils import secure_filename
 
-app = Flask(__name__, template_folder='templates')  # Flask-appen initialiseres med template_folder
-app.config['UPLOAD_FOLDER'] = '/tmp'  # Tilføjer upload mappe konfigurationen
-app.config['ALLOWED_EXTENSIONS'] = {'geojson'}
+app = Flask(__name__, template_folder='templates')
+app.config['UPLOAD_FOLDER'] = '/tmp'
+app.config['ALLOWED_EXTENSIONS'] = {'geojson', 'zip'}
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
@@ -38,6 +41,16 @@ def convert_geojson_to_arcgis(geojson):
         arcgis_features.append(arcgis_feature)
     return arcgis_features
 
+def convert_shapefile_to_geojson(shapefile_path):
+    with shapefile.Reader(shapefile_path) as shp:
+        fields = [field[0] for field in shp.fields if field[0] != 'DeletionFlag']
+        features = []
+        for sr in shp.shapeRecords():
+            attributes = dict(zip(fields, sr.record))
+            geom = sr.shape.__geo_interface__
+            features.append({"type": "Feature", "geometry": geom, "properties": attributes})
+        return {"type": "FeatureCollection", "features": features}
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -56,10 +69,23 @@ def upload_file():
         filename = secure_filename(file.filename)
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(filepath)
-        
-        with open(filepath, 'r') as f:
-            geojson_data = json.load(f)
-        
+
+        if filename.endswith('.geojson'):
+            with open(filepath, 'r') as f:
+                geojson_data = json.load(f)
+        elif filename.endswith('.zip'):
+            with zipfile.ZipFile(filepath, 'r') as zip_ref:
+                zip_ref.extractall(app.config['UPLOAD_FOLDER'])
+                for fname in zip_ref.namelist():
+                    if fname.endswith('.shp'):
+                        shapefile_path = os.path.join(app.config['UPLOAD_FOLDER'], fname)
+                        geojson_data = convert_shapefile_to_geojson(shapefile_path)
+                        break
+                else:
+                    return jsonify({'error': 'No shapefile found in ZIP'})
+        else:
+            return jsonify({'error': 'File type not supported'})
+
         features = convert_geojson_to_arcgis(geojson_data)
         
         url = "https://services6.arcgis.com/QHir1urgnGYroCLG/ArcGIS/rest/services/PG_versioneret_110624/FeatureServer/0/addFeatures"
